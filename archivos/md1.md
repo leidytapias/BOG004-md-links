e70386e832aab9e7e084218b885633c5c9e46d8e
leidy <leidytapiasgiraldo@gmail.com>
2022-04-22 14:53:37 -0500

    Se realiza la lectura de archivos .md con recursividad

---
+++ archivos/md1.md
@@ -0,0 +1,7 @@
+[Markdown1](https://es.wikipedia.org/wiki/Markdown)
+
+[Markdown2](https://es.wikipedia.org/wiki/Markdown)
+
+[Markdown3](https://es.wikipedia.org/wiki/Markdown)
+
+[prueba](https://mysqldbm.com)
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  // ...
-};
+const getFilesWithRecursively = require('./js/recursively.js');   
+const Path = require("path");
+
+const mdLinks = (path, options) => new Promise((resolve, reject) => {
+   const files = getFilesWithRecursively(path);
+  console.log(files);
+})
+
+mdLinks("./archivos", { validate: true });
+
+module.exports = mdLinks;
