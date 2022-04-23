6e633e07775509d4fc46879108977b1f2a5c8d31
leidy <leidytapiasgiraldo@gmail.com>
2022-04-23 18:57:50 -0500

    se transforma los enlaces solo con 3 propiedades

--- index.js
+++ index.js
@@ -16,22 +16,31 @@ const mdLinks = (path, options) => new Promise((resolve, reject) => {
       files.forEach((file) => {
         // Obtener los enlaces de cada archivo de la lista de archivos
         const links = getLinks(file);
+        console.log(links);
       });
     }
   } else if (extension === '.md') {
     // si la extension es igual a .md obtiene los enlaces de ese archivo md
     const links = getLinks(path);
+    console.log(links);
   } else {
     reject('Envié una carpeta o un archivo .md')
   }
 
 });
 
-
 const getLinks = (file) => {
   const markdown = readFileSync(file, { encoding: 'utf8' });
   const { links } = markdownLinkExtractor(markdown, true);
-  return links;
+  // transformar los enlaces solo con las 3 propiedades, ya que links tiene muchas propiedades que no se usuaran
+  const transformedLinks = links.map((link) => {
+    return {
+      href: link.href,
+      text: link.text,
+      file
+    }
+  });
+  return transformedLinks;
 }
 
 
